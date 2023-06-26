65218fe381bad5d4cddae42d31565ee92c4b69dc
Vũ Huy Hoàng <115575325+HiraDuong@users.noreply.github.com>
2023-06-26 22:15:07 +0700

    PUSH
    26/06/2023

--- FE/JS/main.js
+++ FE/JS/main.js
@@ -2,3 +2,8 @@ function avt_click() {
     var content = document.getElementById("avt_container_id");
     content.classList.toggle("hidden");
   }
+
+function account_in4(){
+    var content = document.getElementById("avt_container_id");
+    content.classList.toggle("hidden");
+}
